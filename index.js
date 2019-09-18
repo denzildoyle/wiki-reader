5f95d61e5a355600365680a5442c2c098a8d5b3d
denzildoyle <denzildoyle@gmail.com>
2019-09-18 16:00:01 -0400

    Allow user to enter language

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const fetch = require("node-fetch");
-var langCode = "en";
+const readline = require("readline");
+
 var article = "pizza";
 
-var url = `https://${langCode}.wikipedia.org/w/api.php?action=query&prop=extracts&explaintext=true&titles=${article}&format=json`;
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
-fetch(url)
-.then(response => response.json())
-.then(data => (console.log(data)))
-.catch(err => console.error("Error retrieving article: " + err));
+rl.prompt();
+
+rl.question("Please enter your preferred language eg. en, nl, fr: ", answer => {
+  var langCode = answer;
+  console.log(langCode);
+});
+
+
+// var url = `https://${langCode}.wikipedia.org/w/api.php?action=query&prop=extracts&explaintext=true&titles=${article}&format=json`;
+
+// fetch(url)
+// .then(response => response.json())
+// .then(data => (console.log(data)))
+// .catch(err => console.error("Error retrieving article: " + err));
