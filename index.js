4fc4c1f26cf02b9dacefd9d6af0aeb3c2b3b1a02
denzildoyle <denzildoyle@gmail.com>
2019-09-19 10:49:33 -0400

    Get the command and the article(title) from the terminal

--- index.js
+++ index.js
@@ -6,21 +6,38 @@ var rl = readline.createInterface({
   output: process.stdout
 });
 
-// get the user language preference 
+// get the user language preference
 rl.question("Please enter your preferred language eg. en, nl, fr: ", answer => {
   readLine(answer);
 });
 
 function readLine(langCode) {
-  //TODO: create language function to convert language code to language 
-  console.log(`You have selected to run this application in ${langCode} to read. Use the command READ <article> load an article or HELP to show a full list of options:`);
-  
+  //TODO: create language function to convert language code to language
+  console.log(
+    `You have selected to run this application in ${langCode} to read. Use the command READ <article> load an article or HELP to show a full list of options:`
+  );
+
   rl.prompt();
-  
+
   rl.on("line", function(line) {
-    console.log(line);
+    if (hasWhiteSpace(line) == true) {
+      var lineArray = line.split(" ");
+      command = lineArray[0];
+      article = line.replace(lineArray[0] + " ", "");
+    } else {
+      command = line;
+    }
+
+    console.log("Command:" + command);
+    console.log("Article:" + article);
   });
 }
+
+function hasWhiteSpace(string) {
+  return string.indexOf(" ") >= 0 ? true : false;
+}
+
+function readInput(line) {}
 
 // var article = "pizza";
 // var url = `https://${langCode}.wikipedia.org/w/api.php?action=query&prop=extracts&explaintext=true&titles=${article}&format=json`;
